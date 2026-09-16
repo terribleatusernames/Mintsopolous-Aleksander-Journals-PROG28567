2694292d1a570b32960501f12b8536dfb6ac6edd
mintsopo <mintsopo@OA-S141-22>
2026-09-16 10:31:57 -0400

    Inclass exercise

---
+++ Assets/Scripts/Controllers/Enemy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    
+    private void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/Controllers/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+public class Player : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public List<Transform> asteroidTransforms;
+
+    public Vector3 result;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.bKey.wasPressedThisFrame)
+        {
+            result = AddNumbers(Vector3.up, Vector3.up );
+
+            StartCoroutine(spawnWithDelay(3f));
+
+        }
+
+        Debug.Log(transform.position);
+
+    }
+
+    private IEnumerator spawnWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        SpawnBombAtOffset(result);
+    }
+
+
+    Vector3 AddNumbers(Vector3 one, Vector3 two)
+    {
+        return one - two;
+    }
+
+    void SpawnBombAtOffset(Vector3 inOffset)
+    {
+        Instantiate(bombPrefab, transform.position + inOffset, Quaternion.identity);
+    }
+}
