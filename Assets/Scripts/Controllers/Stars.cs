89881100fb50b2b3fb3f2bb7ddfbbd5469e30793
mintsopo <mintsopo@OA-S141-22>
2026-09-24 22:48:02 -0400

    Task 4

--- Assets/Scripts/Controllers/Stars.cs
+++ Assets/Scripts/Controllers/Stars.cs
@@ -1,5 +1,7 @@
+using Mono.Cecil.Cil;
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class Stars : MonoBehaviour
@@ -11,14 +13,63 @@ public class Stars : MonoBehaviour
     private Vector3 startPosition;
     private Vector3 endPosition;
 
-    // Update is called once per frame
-    void Update()
+    public int starCount = 0;
+
+    private bool isDrawing = false;
+
+    private bool doneDrawing = false;
+
+    private Coroutine drawCoroutine;
+
+    // Start drawing when the object is enabled
+    void Start()
     {
         
     }
 
-    private void DrawConstellation()
+    void Update()
     {
+       if (!isDrawing) {
+            isDrawing = true;
+            drawCoroutine = StartCoroutine(DrawConstellation());
+        }
+
+       if(doneDrawing)
+        {
+            StopCoroutine(drawCoroutine);
+  
+            isDrawing = false;
+            doneDrawing = false;
+        }  
+    }
+
+    private IEnumerator DrawConstellation()
+    {
+   
+        for (int i = 0; i < starTransforms.Count - 1; i++)
+        {
+            startPosition = starTransforms[i].position;
+            endPosition = starTransforms[i + 1].position;
+            
+            float elapsed = 0f;
+
+            while (elapsed < drawingTime)
+            {
+                
+                currentPosition = Vector3.Lerp(startPosition, endPosition, (elapsed / drawingTime));
+
+                Debug.DrawLine(startPosition, currentPosition, Color.white);
+
+
+                elapsed += Time.deltaTime;
+               
+                yield return null;
+            }
+
+            yield return null;
+        }
+
+       doneDrawing = true;
 
     }
 }
